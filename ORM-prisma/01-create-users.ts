c134f00e4af8eca249aadc36e4c7165054b39329
Pradeep Sahu <official.pradeepsahu@gmail.com>
2026-02-01 15:29:21 +0530

    prisma migration script

---
+++ ORM-prisma/01-create-users.ts
@@ -0,0 +1,21 @@
+import { prisma } from "./lib/db";
+
+async function main() {
+  await prisma.user.create({
+    data: {
+      email: "harkirat1@gmail.com",
+      name: "harkriat",
+    },
+  });
+}
+
+main()
+  .then(async () => {
+    console.log("done");
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
